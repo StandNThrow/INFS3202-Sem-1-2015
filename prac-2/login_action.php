f2cbcc11c8363bdb03eb0dde81c0ea3765ad85f8
Ru Chern <iruchern@gmail.com>
2015-04-14 15:21:51 +1000

    Updated login_action.php
    Signed-off-by: Ru Chern <iruchern@gmail.com>

--- prac-2/login_action.php
+++ prac-2/login_action.php
@@ -1,27 +1,29 @@
-<?php /* Starts the session */
+<?php 
+/* Check Login form submitted */
+if(isset($_POST['Submit']))
+{
+	/* Define username and associated password array */
+	$logins = array('INFS' => '3202', 'infs' => '3202');
 	
-	/* Check Login form submitted */	
-	if(isset($_POST['Submit'])){
-		/* Define username and associated password array */
-		$logins = array('INFS' => '3202','infs' => '3202');
+	/* Check and assign submitted Username and Password */
+	$Username = isset($_POST['Username']) ? $_POST['Username'] : '';
+	$Password = isset($_POST['Password']) ? $_POST['Password'] : '';
+	$Timeout = isset($_POST['Timeout']) ? $_POST['Timeout'] : '';
+	
+	/* Check Username and Password existence in defined array */		
+	if (isset($logins[$Username]) && $logins[$Username] == $Password)
+	{
+		/* Success: Set session variables and redirect to Homepage  */
+		$_SESSION['Username']=$Username;
+		$_SESSION['Timeout']=$Timeout;
+		$_SESSION['isLoggedIn']="true";
 		
-		/* Check and assign submitted Username and Password to new variable */
-		$Username = isset($_POST['Username']) ? $_POST['Username'] : '';
-		$Password = isset($_POST['Password']) ? $_POST['Password'] : '';
-		$Timeout = isset($_POST['Timeout']) ? $_POST['Timeout'] : '';
+		header('location:index.php?handler=' . urlencode(base64_encode('You have logged in successfully. Your session will timeout in: ' . $Timeout . 's<br>Welcome back, ' . $_SESSION["Username"])));
 		
-		/* Check Username and Password existence in defined array */		
-		if (isset($logins[$Username]) && $logins[$Username] == $Password){
-			/* Success: Set session variables and redirect to Protected page  */
-			$_SESSION['Username']=$Username;
-			$_SESSION['Timeout']=$Timeout;
-			$_SESSION['isLoggedIn']="true";
-			header('location:index.php?handler=' . urlencode(base64_encode('You have logged in successfully. Your session will timeout in: ' . $Timeout . 's<br>Welcome back, ' . $_SESSION["Username"])));
-			
-			date_default_timezone_set('Australia/Brisbane');
-			$log  = date("Y-m-d H:i") . " " . $Username . " Login" .PHP_EOL;
-			
-			file_put_contents('./log.txt', $log, FILE_APPEND);
+		date_default_timezone_set('Australia/Brisbane');
+		$log  = date("Y-m-d H:i") . " " . $Username . " Login" .PHP_EOL;
+		
+		file_put_contents('./log.txt', $log, FILE_APPEND);
 		} else {
 			/*Unsuccessful attempt: Set error message */
 			$msg="<span style='color:red'>Invalid Login Details</span>";
