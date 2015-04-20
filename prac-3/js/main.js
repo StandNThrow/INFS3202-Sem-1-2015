d70401735490884b9647b6db710072a11725524b
Ru Chern <iruchern@gmail.com>
2015-04-21 03:18:47 +1000

    Updated main.js
    Signed-off-by: Ru Chern <iruchern@gmail.com>

--- prac-3/js/main.js
+++ prac-3/js/main.js
@@ -1,22 +1,3 @@
-/*function initCountdown(countdown) {
-    function getRemainder(time) {
-        if(time < 0) {
-            return '0:0'
-        }
-        var minutes = Math.floor(time / 60);
-        var seconds = time - minutes * 60;
-        return minutes + ':' + seconds;
-    }
-
-    var interval = setInterval(function() {
-        countdown = countdown - 1;
-        document.title = getRemainder(countdown) + ' till your session expires.';
-        if(countdown < 0) {
-			location.href	=	"logout_timeout.php";
-        }
-    }, 1000);
-}*/
-
 $(function() {
     $('.moreInfo').click(function() {
         var button = $(this);
