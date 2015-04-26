dee0336ceb0e96e04862546f36d4e7190a42ac4d
Ru Chern <iruchern@gmail.com>
2015-04-26 17:13:39 +1000

    Updated main.js

--- prac-3/js/main.js
+++ prac-3/js/main.js
@@ -11,3 +11,38 @@ $(function() {
         });
     });
 });
+
+$(document).ready(function() 
+{
+    $("#formEdit").validate({
+        rules: {
+            name: {
+                minlength: 4,
+                required: true
+            },
+            address: {
+                minlength: 4,
+                required: true
+            },
+            phoneno: {
+                minlength: 4,
+                required: true
+            },
+            images: {
+                minlength: 4,
+                required: true,
+            },
+            description: {
+                minlength: 4,
+                required: true
+            }
+            // messages: {
+            //     name: "Please enter name of the restaurant.",
+            //     address: "Please enter address of the restaurant.",
+            //     phoneno: "Plese enter a contact number of the restaurant.",
+            //     images: "Please enter valid image links.",
+            //     description: "Please enter a decription of the restaurant."
+            // }
+        }
+    });
+});
