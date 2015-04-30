eda89fe51b46bdccf5266359e4193ee22425aab3
Ru Chern <iruchern@gmail.com>
2015-05-01 04:04:25 +1000

    Added stuff to search_action.php

--- prac-4/search_action.php
+++ prac-4/search_action.php
@@ -1,6 +1,23 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="initial-scale=1, maximum-scale=1">
+	<title>Search Action</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/lightbox.css">
+	<link rel="shortcut icon" href="images/favicon.png">
+	<script src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
+	<script src="js/main.js"></script>
 	<?php require("db_config.php"); ?>
 </head>
 
@@ -64,5 +81,25 @@
 		echo "</table>";
 	}
 	?>
+	<script src="//code.jquery.com/jquery-1.11.2.min.js"></script> 
+	<!-- Latest compiled and minified JavaScript --> 
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script> 
+	<script src="js/lightbox.js"></script> 
+	<script src="js/googleMapAPI.js"></script>
+	<script>
+	/* Hide/Show More Info */
+	$(document).ready(function() {
+		$('.moreInfo').click(function() {
+			var button = $(this);
+			$(button).closest('.more-panel').find('.moreInfo-panel').slideToggle('fast', function() {
+				if ($(this).is(':visible')) {
+					button.text('Hide');
+				} else {
+					button.text('More Info');
+				}
+			});
+		});
+	});
+	</script>
 </body>
 </html>
