ecc57a087a24cc6b851acb093dfa50c3d436128c
Ru Chern <iruchern@gmail.com>
2015-05-15 06:25:20 +1000

    Add stuff.

---
+++ prac-6/azure_db_config.php
@@ -0,0 +1,15 @@
+<?php
+
+$server = "tcp:bdkzfvt0vn.database.windows.net,1433";
+$user = "infs3202_admin@bdkzfvt0vn";
+$pwd = "Passworda@";
+$db = "infs3202";
+
+$conn = sqlsrv_connect($server, array("UID"=>$user, "PWD"=>$pwd, "Database"=>$db));
+
+if($conn === false){
+	die(print_r(sqlsrv_errors()));
+}
+
+echo "Connected successfully.";
+?>
