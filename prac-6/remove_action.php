290fe616155538c10098d379d3f6eeeb956584f7
Ru Chern <iruchern@gmail.com>
2015-05-18 23:06:08 +1000

    Debug

--- prac-6/remove_action.php
+++ prac-6/remove_action.php
@@ -3,8 +3,7 @@ require("azure_db_config.php");
 
 $id = $_POST["id"];
 
-$sql = "DELETE FROM markers WHERE id='$id'";
-// $result	= mysqli_query($connect, $sql);
+$sql = "DELETE FROM markers WHERE id=\"" . $id . "\"";
 $result = $conn->query($sql);
 
 header("Location: admin.php");
