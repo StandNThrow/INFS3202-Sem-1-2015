117fe7a5673e2e61dab2b17a7e5a4fd187447e58
Ru Chern <iruchern@gmail.com>
2015-05-18 22:36:19 +1000

    Debug

--- prac-6/edit.php
+++ prac-6/edit.php
@@ -35,6 +35,7 @@
 	$result = $sql->execute(array(":id" => $id));
 
 	if ($result->rowCount() > 0) {
+		print_r($result)
 		$row = $result->fetch();
 		print_r($row);
 	// echo "<div class=\"modal-body\">";
