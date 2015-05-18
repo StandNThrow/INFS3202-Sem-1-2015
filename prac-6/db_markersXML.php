4b94f55645da383416a25adf0269dd026d748980
Ru Chern <iruchern@gmail.com>
2015-05-19 00:02:17 +1000

    Debug

--- prac-6/db_markersXML.php
+++ prac-6/db_markersXML.php
@@ -3,14 +3,13 @@ require("azure_db_config.php");
 
 // Start XML file, create parent node
 $dom = new DOMDocument("1.0");
+header("Content-type: text/xml");
 $node = $dom->createElement("markers");
 $parnode = $dom->appendChild($node);
 
 // Select all the rows in the markers table
 $sql = "SELECT * FROM markers";
 $result = $conn->query($sql);
-
-header("Content-type: text/xml");
 
 // Iterate through the rows, adding XML nodes for each
 while ($row = $result->fetch()) {
