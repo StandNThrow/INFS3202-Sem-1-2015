1b98f8d69e23c67475e4db8e49e1340fe0bc7839
Ru Chern <iruchern@gmail.com>
2015-05-18 23:25:07 +1000

    Debug

--- prac-6/db_markersXML.php
+++ prac-6/db_markersXML.php
@@ -13,21 +13,19 @@ $result = $conn->query($sql);
 header("Content-type: text/xml");
 
 // Iterate through the rows, adding XML nodes for each
-while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-	echo $row["id"], $row["name"], $row["address"];
+while ($row = $result->fetch()) {
 // ADD TO XML DOCUMENT NODE
-	// $node = $dom->createElement("marker");
-	// $newNode = $parnode->appendChild($node);
-	// $newNode->setAttribute("id", $row["id"]);
-	// $newNode->setAttribute("name", $row["name"]);
-	// $newNode->setAttribute("address", $row["address"]);
-	// $newNode->setAttribute("contact", $row["contact"]);
-	// $newNode->setAttribute("image", $row["imgURL"]);
-	// $newNode->setAttribute("lat", $row["lat"]);
-	// $newNode->setAttribute("lng", $row["lng"]);
-	// $newNode->setAttribute("description", $row["description"]);
+	$node = $dom->createElement("marker");
+	$newNode = $parnode->appendChild($node);
+	$newNode->setAttribute("id", $row->"id");
+	$newNode->setAttribute("name", $row["name"]);
+	$newNode->setAttribute("address", $row["address"]);
+	$newNode->setAttribute("contact", $row["contact"]);
+	$newNode->setAttribute("image", $row["imgURL"]);
+	$newNode->setAttribute("lat", $row["lat"]);
+	$newNode->setAttribute("lng", $row["lng"]);
+	$newNode->setAttribute("description", $row["description"]);
 }
-
 var_dump($dom);
 //echo $dom->saveXML();
 ?>
