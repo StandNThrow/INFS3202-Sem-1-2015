9fa1b42daa9a6f8276f5a1e59ac029a389714c94
Ru Chern <iruchern@gmail.com>
2015-05-18 23:56:01 +1000

    Debug

--- prac-6/db_markersXML.php
+++ prac-6/db_markersXML.php
@@ -2,31 +2,28 @@
 require("azure_db_config.php");
 
 // Start XML file, create parent node
-// $dom = new DOMDocument("1.0");
-// $node = $dom->createElement("markers");
-// $parnode = $dom->appendChild($node);
+$dom = new DOMDocument("1.0");
+$node = $dom->createElement("markers");
+$parnode = $dom->appendChild($node);
 
 // Select all the rows in the markers table
 $sql = "SELECT * FROM markers";
 $result = $conn->query($sql);
 
-var_dump($result->fetch());
-
-// header("Content-type: text/xml");
+header("Content-type: text/xml");
 
 // Iterate through the rows, adding XML nodes for each
 while ($row = $result->fetch()) {
-// ADD TO XML DOCUMENT NODE
 	$node = $dom->createElement("marker");
 	$newNode = $parnode->appendChild($node);
 	$newNode->setAttribute("id", $row["id"]);
-	$newNode->setAttribute("name", $row["name"]);
-	$newNode->setAttribute("address", $row["address"]);
-	$newNode->setAttribute("contact", $row["contact"]);
-	$newNode->setAttribute("image", $row["imgURL"]);
-	$newNode->setAttribute("lat", $row["lat"]);
-	$newNode->setAttribute("lng", $row["lng"]);
-	$newNode->setAttribute("description", $row["description"]);
+	// $newNode->setAttribute("name", $row["name"]);
+	// $newNode->setAttribute("address", $row["address"]);
+	// $newNode->setAttribute("contact", $row["contact"]);
+	// $newNode->setAttribute("image", $row["imgURL"]);
+	// $newNode->setAttribute("lat", $row["lat"]);
+	// $newNode->setAttribute("lng", $row["lng"]);
+	// $newNode->setAttribute("description", $row["description"]);
 }
 echo $dom->saveXML();
 ?>
