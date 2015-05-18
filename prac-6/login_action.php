5996cb70cacfd376346d94decb1315e1a9b45d3f
Ru Chern <iruchern@gmail.com>
2015-05-18 22:53:54 +1000

    Code cleanup

--- prac-6/login_action.php
+++ prac-6/login_action.php
@@ -5,8 +5,7 @@ date_default_timezone_set("Australia/Brisbane");
 session_start();
 
 /* Check Login form submitted */
-if(isset($_POST["submit"]))
-{
+if(isset($_POST["submit"])) {
 	$username = $_POST["username"];
 	$password = $_POST["password"];
 
@@ -14,7 +13,9 @@ if(isset($_POST["submit"]))
 	$result->bindParam(":username", $username);
 	$result->bindParam(":password", $password);
 	$result->execute();
+
 	$rows = $result->fetch(PDO::FETCH_NUM);
+
 	if ($rows > 0) {
 		echo $username, $password;
 		$_SESSION['username'] = $username;
@@ -22,29 +23,5 @@ if(isset($_POST["submit"]))
 	} else {
 		echo "Invalid login";
 	}
-	// /* Define username and associated password array */
-	// $logins = array("admin" => "password");
-	
-	// /* Check and assign submitted Username and Password */
-	// $Username = isset($_POST["username"]) ? $_POST["username"] : "";
-	// $Password = isset($_POST["password"]) ? $_POST["password"] : "";
-	
-	// /* Check Username and Password existence in defined array */		
-	// if (isset($logins[$Username]) && $logins[$Username] == $Password)
-	// {
-	// 	/* Success: Set session variables and redirect to Homepage  */
-	// 	$_SESSION['username'] = $Username;
-
-	// 	header("Location: admin.php");
-
-	// 	// $log  = date("Y-m-d H:i") . " " . $Username . " Login" . PHP_EOL;
-
-	// 	// file_put_contents("../logs/log.txt", $log, FILE_APPEND);
-	// } else {
-	// 	/*Unsuccessful attempt: Set error message */
-	// 	$msg="<span style=\"color:red;\">Invalid Login Details!</span>";
-	// 		// $log  = date("Y-m-d H:i") . " " . $Username . " Failed to login" . PHP_EOL;
-	// 		// file_put_contents("../logs/log.txt", $log, FILE_APPEND);
-	// }
 }
 ?>
