5745daf30c761e2181476d0c925c55572d07ac86
Ru Chern <iruchern@gmail.com>
2015-05-18 16:36:55 +1000

    Updated db_markersXML.php to fit PDO

--- prac-6/db_markersXML.php
+++ prac-6/db_markersXML.php
@@ -1,5 +1,5 @@
 <?php
-require("db_config.php");
+require("azure_db_config.php");
 
 // Start XML file, create parent node
 $dom = new DOMDocument("1.0");
@@ -8,7 +8,8 @@ $parnode = $dom->appendChild($node);
 
 // Select all the rows in the markers table
 $sql = "SELECT * FROM markers";
-$result = mysqli_query($connect, $sql);
+// $result = mysqli_query($connect, $sql);
+$result = $conn->$query($sql);
 if (!$result) {
 	die('Invalid query: ' . mysql_error());
 }
@@ -17,7 +18,7 @@ header("Content-type: text/xml");
 
 // Iterate through the rows, adding XML nodes for each
 //while ($row = @mysql_fetch_array($result)) {
-while ($row = $result->fetch_object()) {
+while ($row = $result->fetch()) {
 // ADD TO XML DOCUMENT NODE
 	$node = $dom->createElement("marker");
 	$newNode = $parnode->appendChild($node);
